05a419201e5b298ad0485b39e20381e39cd16ed0
Damilola Onitiju <damilolaonitiju28@gmail.com>
2023-11-19 23:24:08 +0100

    last stat promise

--- index.js
+++ index.js
@@ -14,18 +14,50 @@ const heroSlider = new Swiper(".cc-home_slide", {
 
 //stat display section
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   let statDisplays = document.querySelectorAll(".num");
+//   let interval = 5000;
+
+//   statDisplays.forEach((statDisplay) => {
+//     let startValue = 0;
+//     let endValue = parseInt(statDisplay.getAttribute("data-val"));
+//     let duration = Math.floor(interval / endValue);
+//     let counter = setInterval(function () {
+//       startValue += 1;
+//       statDisplay.textContent = startValue;
+//       if (startValue == endValue) {
+//         clearInterval(counter);
+//       }
+//     }, duration);
+//   });
+// });
+
+// //stat display with odometer
+// let value = 12345;
+// setInterval(() => {
+//   odometer.innerHtml = value;
+
+// }, 3000)
+
 document.addEventListener("DOMContentLoaded", function () {
   let statDisplays = document.querySelectorAll(".num");
-  let interval = 5000;
 
   statDisplays.forEach((statDisplay) => {
-    let startValue = 0;
     let endValue = parseInt(statDisplay.getAttribute("data-val"));
+    let odometerInstance = new Odometer({
+      el: statDisplay,
+      value: 0, // Initial value
+      format: "(,ddd)", // Format for thousand separators
+    });
+
+    let interval = 5000;
     let duration = Math.floor(interval / endValue);
+
     let counter = setInterval(function () {
+      odometerInstance.update(startValue);
       startValue += 1;
-      statDisplay.textContent = startValue;
-      if (startValue == endValue) {
+
+      if (startValue > endValue) {
         clearInterval(counter);
       }
     }, duration);
