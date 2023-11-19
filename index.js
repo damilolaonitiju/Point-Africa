f1c360f2e40fb4770dd3d275b8abdb3cb603dd15
Damilola Onitiju <damilolaonitiju28@gmail.com>
2023-11-19 22:03:35 +0100

    stat value counter js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const heroSlider = new Swiper(".cc-home_slide", {
   // Optional parameters
   loop: true,
@@ -11,4 +10,22 @@ const heroSlider = new Swiper(".cc-home_slide", {
     nextEl: ".cc-arrow_forward",
     prevEl: ".cc-arrow_back",
   },
+});
+
+//stat display section
+
+let statDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+statDisplays.forEach((statDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(statDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    statDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
